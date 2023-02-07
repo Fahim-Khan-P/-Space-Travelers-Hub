91bdf699f74b22d17eac1f2d5a68185cf0e98654
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-02-07 21:50:43 +0430

    fetch and display rockets!

---
+++ src/components/Rocket.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { reserveRocket } from './redux/Rockets/Rockets';
+
+const Rocket = (rocket) => {
+  const {
+    flickrImages, rocketName, description, id, reserved,
+  } = rocket;
+
+  console.log(rocket);
+  const dispatch = useDispatch();
+  const handleReserve = () => {
+    dispatch(reserveRocket(id));
+  };
+
+  return (
+    <div className="rocketContainer" id={id}>
+      <img className="rocketImage" alt="rocket..." src={flickrImages} />
+      <div className="rocketDetails">
+        <h2 className="rocketName">{rocketName}</h2>
+        <p className="rocketDescription">{description}</p>
+
+        <button
+          type="button"
+          className="reserveBtn"
+          onClick={handleReserve}
+        >
+          {reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Rocket;
--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,5 +1,31 @@
-const Rockets = () => (
-  <h1>Welcome user, This is a Rockets Page!</h1>
-);
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Rocket from './Rocket';
+import { fetchRockets } from './redux/Rockets/Rockets';
 
+const Rockets = () => {
+  const rocketList = useSelector((state) => state.rockets);
+  const state = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (state.length === 0) {
+      dispatch(fetchRockets());
+    }
+  }, [dispatch, state]);
+
+  return (
+    <div className="allRocketsContainer">
+      {rocketList.map((item) => (
+        <Rocket
+          key={item.id}
+          id={item.id}
+          flickrImages={item.flickrImages}
+          rocketName={item.rocketName}
+          description={item.description}
+          reserved={item.reserved}
+        />
+      ))}
+    </div>
+  );
+};
 export default Rockets;
